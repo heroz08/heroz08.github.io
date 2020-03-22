a75b05970b213b6d39b36dc705381de1fa904762
hzhyang <zilongerdai@gmail.com>
2020-03-22 14:25:12 +0800

    create new article

--- pushPage.sh
+++ pushPage.sh
@@ -1,5 +1,9 @@
 cp /Users/hzhyang/Documents/markdown/baidu_verify_91MrJoVVBN.html /Users/hzhyang/Documents/markdown/public
+wait
+echo "cp done"
 hexo d
+wait
+echo "start push page"
 git add -A
 git commit -m 'create new article'
 git pull
