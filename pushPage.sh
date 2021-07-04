a0dadacd64fa62ac37f458df0290d1fb452e0cd8
hzhyang <zilongerdai@google.com>
2021-07-04 18:12:40 +0800

    create new article

--- pushPage.sh
+++ pushPage.sh
@@ -3,7 +3,7 @@ echo "clean done"
 hexo g
 wait
 echo "hexo g done"
-cp ./baidu_verify_91MrJoVVBN.html /root/heroz08.github.io/public/
+cp ./baidu_verify_91MrJoVVBN.html /public/
 sleep 5
 wait
 echo "cp done"
@@ -13,5 +13,13 @@ git add -A
 git commit -m 'create new article'
 git pull
 git push
-
-hexo d
+sleep 3
+wait
+echo 'push done'
+mv ./public ../public
+echo 'temp'
+git checkout .
+git checkout master
+rm -rf *
+mv ../public/* .
+echo 'mv done'
